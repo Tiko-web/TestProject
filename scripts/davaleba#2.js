ee630b34dd729395a2e964e1c28469bbc5f5d63c
Tiko-web <tinatingogiladze@gmail.com>
2021-11-07 17:25:39 +0400

    js

---
+++ scripts/davaleba#2.js
@@ -0,0 +1,48 @@
+// // davaleba#1
+let arr = ['tiko', 1, true];
+const s = 27;
+console.log(Array.isArray(arr));
+console.log(Array.isArray(s));
+
+
+// // davaleba#2
+// let arr1 = [1, 2, 3, [4, 5, 6, 7]];
+// console.log(arr1.flat());
+
+
+// // davaleba#3
+// let b = [1, 'two', true];
+// console.log(b[b.length -1]);
+
+
+// // davaleba#4
+// let arr2 = [1,true, undefined, null];
+// console.log(typeof arr2.toString());
+
+// davaleba#5
+// let s = 'hello world';
+// const arr = s.split('w')
+// console.log(arr.join('*'))
+
+// // davaleba#6
+// let  c = "I'm gonna pop some tags only got twenty dollars in my pocket";
+// let d = c.split();
+// console.log(d);
+
+// davaleba#7
+// let arr = [1,2,3,4];
+// const last = arr.pop()
+// const first = arr.unshift(last)
+// console.log(arr)
+
+
+// davaleba#8
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [10, 34, 25, 11];
+// let arr3 = [true, undefined, 'tiko'];
+// const arr = arr1.concat(arr2,arr3)
+// console.log(arr)
+
+// davaleba#9
+// const s = String.fromCharCode(107)
+// console.log(s)
---
+++ scripts/script.js
@@ -0,0 +1,408 @@
+// var n = 1;
+
+// console.log(n);
+
+// n = "2"
+
+// console.log(n);
+
+// var a = 1;
+// var b = 3;
+
+// var sum = a % b;
+
+// console.log(sum);
+
+// var a = 0.1;
+// var b = 0.2;
+// var sum = (a * 10 + b * 10);
+
+// console.log(sum);
+
+// var firstName = "Beqa";
+// var lastName = "Sanaia";
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// var a = undefined;
+// var b = null;
+// var c = NaN;
+// var firstName = "Beqa";
+
+// var n = null;
+// console.log(n);
+
+// console.log(  isNaN(firstName * 3 ) )
+
+// type
+
+// var a = 5;
+// var b = "10";
+// var c = a + 15 + b;
+// console.log(c);
+
+// var a = 5;
+// var b = 10;
+// var c = "30";
+// var d = c + a + b;
+// console.log(d);
+
+// var a = undefined;
+// console.log (typeof a);
+
+// console.log ("string" + " " + undefined);
+
+// var a = ' "string" ';
+// console.log(a);
+
+// var a= "5";
+// var b = "10";
+// console.log(a + b);
+// console.log (typeof (a + b));
+
+// var a = 2;
+// var b = 0;
+// console.log(a/ b);
+
+// var n = 25;
+// var a = n.toString(10);
+
+// var n = 1;
+// n++;
+// console.log(n)
+// console.log(n++)
+// console.log(n)
+// console.log(++n)
+// console.log(n);
+
+// n += 1;
+// console.log(n);
+
+// let a = 5;
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+// console.log(a);
+
+// const a = 5;
+
+// let a = "beqa";
+// let b = "sanaia";
+// const fullName = `${a} ${b}`;
+// console.log(fullName)
+
+// let c = 5;
+// console.log( typeof'${c}')
+
+// let a = 5;
+// let b = "5"
+// console.log(a == b)
+
+// let a = 4;
+
+// if ( a === 7 ) {
+//     console.log('equals 7')
+// } else if (a ===4) {
+//     console.log('equals 4')
+// } else  {
+//     console.log ('you are great')
+// }
+
+// let a = 10;
+// if (a === 3 && a <= 10){
+//     console.log ('true')
+// }
+
+// let a = 10;
+// let b = 12;
+// if ( !!1) {
+//     console.log('ttt')
+// }
+
+// let x= 5;
+// switch (new Date().getDay()) {
+//     case 1:
+//         console.log ('monday');
+//         break;
+//     case 5:
+//         console.log('tuesday');
+//         break;
+//     default:
+//         console.log (ttt);
+//         break;
+// }
+
+// let x = 6;
+// if(x === 5) {
+//     console.log('5')
+// }else {
+//     console.log ('not 5')
+// }
+
+// let x = 6;
+
+// x === 5 ? console.log('5') : console.log('not 5')
+
+// gamravlebis tabula 4 xazshi
+
+// var a = undefined; (declared magram shignit araferi ar WebGLUniformLocation.mag: var n;)
+// var b = null;(mnishvneloba carielia.mag : var n = null;)
+// var c = NaN;
+
+// const a = 400;
+// const b = 500;
+// const c = 200;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > c) {
+//   console.log(b);
+// } else {
+//   console.log(c);
+// }
+
+// const John = 92;
+// const David = 87;
+// const Kate = 55;
+// const Chris = 65;
+
+// if (John >= 90) {
+//   console.log("A");
+// } else if (John >= 80) {
+//   console.log("B");
+// } else if (John >= 70) {
+//   console.log("C");
+// } else if (John >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// if (David >= 90) {
+//   console.log("A");
+// } else if (David >= 80) {
+//   console.log("B");
+// } else if (David >= 70) {
+//   console.log("C");
+// } else if (David >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// if (Kate >= 90) {
+//   console.log("A");
+// } else if (Kate >= 80) {
+//   console.log("B");
+// } else if (Kate >= 70) {
+//   console.log("C");
+// } else if (Kate >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// if (Chris >= 90) {
+//   console.log("A");
+// } else if (Chris >= 80) {
+//   console.log("B");
+// } else if (Chris >= 70) {
+//   console.log("C");
+// } else if (Chris >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// for (let i = 1; i <=10 ; i++ ) {
+//     for (let x = 1; x <= 10; x++){
+//         console.log(i * x)
+//     }
+//  }
+
+// console.log('342')
+// throw new Error ('warning')
+// console.log('shshs');
+
+// throw new SyntaxError('warning')
+
+// try {
+//   throw new Error ('error message')
+// } catch(err) {
+//   console.log("error occured");
+// }
+
+// try {
+//   throw new SyntaxError ('error message')
+// } catch(err) {
+//   console.log(err.name);
+// }
+
+// let s = 'hello';
+// console.log(s.length)
+
+
+// try {
+//     throw new SyntaxError ('error message')
+// } catch(err){
+//     console.log(err.name);
+//     console.log(err.message);
+//     console.log(err.stack);
+// }
+
+// try {
+//     console.log('try block');
+//     throw new Error ('error')
+// } catch (err) {
+//     console.log('catch block');
+//     console.log(err.message)
+// }finally {
+//     console.log ('in finally block')
+// }
+
+
+// let s = 'hello';
+// console.log(s.length);
+
+// let s = 'hello';
+// console.log(s[s.length - 1]);
+// let x = s.length -1;
+// console.log(s[x])
+
+// let s = 'hello'
+// const lastIndex = s.length - 1;
+// console.log(s.charAt(lastIndex))
+
+// let s = 'hello world';
+// console.log(s.substring(2))
+
+// let s = 'hello world';
+// console.log(s.split(' '))
+
+// davaleba#5
+// let s = 'hello world';
+// const arr = s.split('w')
+// console.log(arr.join('*'))
+
+// davaleba#3
+// let s = [1, 'two', true];
+// console.log(s[s.length -1])
+
+// let s= 'hello world';
+// const substring = s.slice(1, 3);
+// console.log(s)
+// console.log(substring)
+
+// davaleba#9
+// const s = String.fromCharCode(107)
+// console.log(s)
+
+// let s = '     string     ';
+// console.log(s.trim().length); (spacebs aclis da marto simboloebs tovebs)
+
+
+// let arr = [1, 2, 3, 'string', true];
+// console.log(arr[0])
+// console.log(arr);
+
+
+// let arr = new Array(1, 2, 3)
+// console.log(arr)
+
+// let arr = [1, 2, 3, 'string', true];
+// arr[0] = 5;
+// console.log(arr)
+
+// let arr = [1, 2, 3, 'string', true];
+// console.log(typeof arr);
+
+// let arr = [1, 2, 3, 'string', true];
+// const a  = '5'
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(a));
+
+// davaleba#1
+// let arr = ['tiko', 1, true];
+// const s = 27;
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(s));
+
+
+// let arr = [1];
+// let arr1 = [1];
+// console.log(arr == arr1);
+// console.log (arr === arr1)
+
+// let arr = Array.of([1, 2, 3], true, 'string');
+// console.log(arr)
+
+// let arr = [1, 3, 8, [true, 'tiko', 7]]
+// console.log(arr)
+
+
+// let arr = [1, 2, 3, 4, 5];
+// const x = arr.filter((a) => {
+//     return a > 3;
+// })
+// console.log(x)
+
+// let arr = [1, 2, 3, 4, 5];
+// const x = arr.map((a) => {
+//     return a ** 2;
+// })
+// console.log(x)
+
+// let arr = [1,2,3,4,5];
+// let a = arr.reduce((c, p) =>{
+//     return c + p;
+// })
+// console.log(a)
+
+// let arr  = [1, 2, 3, 4, 5];
+// const x = arr.every((n) =>{
+// return n >3
+// })
+// console.log(x)
+
+
+
+// let arr = [1, 2, 3, 4];
+// arr.fill(0,2,3)
+// console.log(arr)
+
+// davaleba#7
+// let arr = [1,2,3,4];
+// const last = arr.pop()
+// const first = arr.unshift(last)
+// console.log(arr)
+
+
+// davaleba#8
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [10, 34, 25, 11];
+// let arr3 = [true, undefined, 'tiko'];
+// const arr = arr1.concat(arr2,arr3)
+// console.log(arr)
+
+// davaleba#2
+// let arr1 = [1, 2, 3, [4, 5, 6, 7]];
+// console.log(arr1.flat())
+
+// let arr = [1,2,3,4]
+// const item = arr.find(())
+
+
+// davaleba#6
+// let  a = "I'm gonna pop some tags only got twenty dollars in my pocket";
+// let b = a.split();
+// console.log(b)
+
+
+// davaleba#4
+let arr1 = [1,true, 'undefined', null];
+let arr2 = arr1.toString();
+console.log( typeof arr2)
+
+
